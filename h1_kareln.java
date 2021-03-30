1f931841aced1e7a3602c4505c6ebb1cf02efbcc
KarelNurm <75736496+KarelNurm@users.noreply.github.com>
2021-03-30 11:40:16 +0300

    Add files via upload

---
+++ h1_kareln.java
@@ -0,0 +1,25 @@
+
+public class h1_kareln {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * Prindiv v‰lja nime ja kursuse
+		 */
+		
+		System.out.println("Karel Nurmetalo IT20");
+		
+		//t‰isarv komaarv ja sıne
+		int vanus = 16;
+		double pikkus = 1.70;
+		String hyydnimi = "Kaku";
+		System.out.println(vanus + " " + pikkus + " " + hyydnimi);
+		
+		//muutuja ilma v‰‰rtuseta
+		int minuArv = 0;
+		System.out.println(minuArv);
+	}
+	
+	
+	
+}
